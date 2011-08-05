cd25d0e7aa31141147ad0823f9036b75bce24fd8
Jack Mitchell <jack@communistcode.co.uk>
2011-08-05 11:39:08 +0100

    latest blog version

--- includes/template.inc.php
+++ includes/template.inc.php
@@ -1,20 +1,9 @@
-<?php 
-
-	require_once("../config/config.php");
-	require_once("classes/blogTools.class.php");
-	require_once($cmsPath.'/classes/pageTools.class.php');
-
-	$pageTools = new pageTools();
-	$blogTools = new blogTools();
-
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<title>CCCMS : Blog</title>
+		<title><?php echo($title); ?> : Blog</title>
 		<link href="../stylesheet/stylesheet.css" rel="stylesheet" type="text/css" />
 		<link href="stylesheet/blogStyle.css" rel="stylesheet" type"text/css" />
 	</head>
@@ -32,7 +21,9 @@
 			</div>
 			<div class='blogLinks'>
 
-				<p>Blog links</p>
+				<?php
+					require_once("includes/blogLinks.inc.php");
+				?>
 
 			</div>
 			<div class="blogBody">
@@ -41,8 +32,7 @@
 
 				<?php 
 
-					$blogTools->renderBlogPost($blogTools->getBlogPost($_GET['postID']));
-							
+						echo($content);	
 				?>
 
 			</div>
