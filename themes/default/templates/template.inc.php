6ccaa0ef010d776c853c1ab448eb01d271d416f1
Jack Mitchell <jack@communistcode.co.uk>
2012-02-27 17:50:43 +0000

    fixed weird permissions

--- themes/default/templates/template.inc.php
+++ themes/default/templates/template.inc.php
@@ -1,24 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
 		<title><?php echo($title); ?> : Blog</title>
-		<link href="../themes/<?php echo($pageTools->getTheme("base")); ?>/stylesheets/base.css" rel="stylesheet" type="text/css" />
-		<link href="themes/<?php echo($pageTools->getTheme("blog")); ?>/stylesheets/blogStyle.css" rel="stylesheet" type"text/css" />
+
+    <link href="../themes/<?php echo($pageTools->getTheme("base")); ?>/stylesheets/cssReset.css" rel="stylesheet" />
+    <link href="../themes/<?php echo($pageTools->getTheme("base")); ?>/stylesheets/base.css" rel="stylesheet" />
+		<link href="themes/<?php echo($pageTools->getTheme("blog")); ?>/stylesheets/blogStyle.css" rel="stylesheet" />
+
 	</head>
+
 	<body>
+
 		<div id="mainContainer">
+
 			<div id="title">
+
 				<?php 
 					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/title.inc.php"); 
-			?>
+  			?>
 			</div>
-			<div id='navBar'>
-				<?php 
+	
+  		<div id='navBar'>
+	
+  			<?php 
 					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/links.inc.php"); 
 				?>
-			</div>
+	
+  		</div>
 
 			<div id='bodyContainer'>
 
@@ -56,11 +67,17 @@
 
 			</div>
 
-			<div id="footer">
-				<?php 
-					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/footer.inc.php"); 
-				?>
-			</div>
-		</div>
-	</body>
+   	</div>
+
+		<div id="footer">
+		
+   		<?php 
+				require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/footer.inc.php"); 
+			?>
+	
+	
+  	</div>
+	
+  </body>
+
 </html>
